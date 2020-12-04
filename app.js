3509467520c0731536d1cf356a72768d275518f5
Johan Gavelan <johangavelan@gmail.com>
2020-12-03 20:38:53 -0500

    layout improved

---
+++ app.js
@@ -0,0 +1,4 @@
+const $input = document.querySelector('.todo__input');
+const $add = document.querySelector('todo__add');
+const $list = document.querySelector('.todo__list');
+const $task = document.querySelector('todo__task');
