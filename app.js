528c00d90f4fef01dd73fc6aa7a4c7335429db35
Johan Gavelan <johangavelan@gmail.com>
2020-12-03 21:42:36 -0500

    click button add new task to the list functionality ready

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 const $input = document.querySelector('.todo__input');
-const $add = document.querySelector('todo__add');
+const $add = document.querySelector('.todo__add');
 const $list = document.querySelector('.todo__list');
-const $task = document.querySelector('todo__task');
+const $task = document.querySelector('.todo__task');
+
+const insertTask = () => {
+    $add.addEventListener('click', () => {
+        const newTask = createNewTask($input.value);
+        $list.appendChild(newTask);
+        $input.value = '';
+    })
+}
+
+const createNewTask = elem => {
+    const task = document.createElement('li');
+    task.classList.add('todo__task');
+    
+    task.innerHTML = `
+    <label>
+        <p>${elem}</p>
+        <input type="checkbox" class="todo__check">
+        <i></i>
+    </label>
+    `
+    return task;
+}
