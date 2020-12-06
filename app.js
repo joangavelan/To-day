89c670de8883002b71bb03c65af26b4f773c57a7
Johan Gavelan <johangavelan@gmail.com>
2020-12-06 17:17:24 -0500

    Revealing module pattern implemented

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
-const $input = document.querySelector('.todo__input');
-const $add = document.querySelector('.todo__add');
-const $list = document.querySelector('.todo__list');
+const App = (() => {
+    const $input = document.querySelector('.todo__input');
+    const $add = document.querySelector('.todo__add');
+    const $list = document.querySelector('.todo__list');
 
+    const createNewTask = elem => {
+        const task = document.createElement('li');
+        task.classList.add('todo__task');
+        
+        task.innerHTML = `
+        <label>
+            <input type="checkbox" class="todo__check">
+            <p>${elem}</p>
+            <i></i>
+        </label>
+        `
+        return task;
+    }
 
-const createNewTask = elem => {
-    const task = document.createElement('li');
-    task.classList.add('todo__task');
+    const insertTask = () => {
+        if($input.value.length === 0) {
+            alert('Write something.')
+        }
+        else {
+            const newTask = createNewTask($input.value);
+            $list.insertBefore(newTask, $list.childNodes[0]);
+            $input.value = '';
+        }
+    }
+
+    const listeners = () => {
+        $add.addEventListener('click', () => insertTask());
+        $input.addEventListener('keypress', event => event.keyCode === 13 ? insertTask() : false );
+    }
     
-    task.innerHTML = `
-    <label>
-        <input type="checkbox" class="todo__check">
-        <p>${elem}</p>
-        <i></i>
-    </label>
-    `
-    return task;
-}
 
-const insertTask = () => {
-    if($input.value.length === 0) {
-        alert('Write something.')
+    return {
+        listeners
     }
-    else {
-        const newTask = createNewTask($input.value);
-        $list.insertBefore(newTask, $list.childNodes[0]);
-        $input.value = '';
-    }
-}
 
+})();
 
-$add.addEventListener('click', () => insertTask());
-$input.addEventListener('keypress', event => event.keyCode === 13 ? insertTask() : false );
+App.listeners();
